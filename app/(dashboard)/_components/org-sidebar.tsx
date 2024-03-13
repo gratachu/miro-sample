aaeab710ba6ba477e2d884d9cd827187a405a2e3
TatsuakiIto <gratachu9@gmail.com>
2024-03-14 08:30:26 +0900

    update: org sidebar

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,9 +1,34 @@
 "use client"
 
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+import Image from "next/image";
+import {cn} from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+});
+
 export const OrgSidebar = () => {
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500">
-      Org Sidebar
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+      <Link href={"/"}>
+        <div className="flex items-center gap-x-2">
+          <Image
+            src={"/logo.svg"}
+            alt={"Logo"}
+            height={60}
+            width={60}
+          />
+          <span className={cn(
+            "font-semibold text-2xl",
+            font.className
+          )}>
+            Board
+          </span>
+        </div>
+      </Link>
     </div>
   )
 }
