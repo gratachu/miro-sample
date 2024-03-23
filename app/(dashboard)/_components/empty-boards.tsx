8e8367b486b834bf6440b8c5e3842f935ca33107
TatsuakiIto <gratachu9@gmail.com>
2024-03-23 18:08:38 +0900

    add: create mutation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,24 @@
+"use client"
+
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import {useOrganization} from "@clerk/nextjs";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization()
+  const create = useMutation(api.board.create)
+
+  const onClick = () => {
+    if(!organization) return
+
+    create({
+      title: "Untitled",
+      orgId: organization.id,
+    })
+  }
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image
@@ -17,7 +34,7 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button size="lg">
+        <Button size="lg" onClick={onClick}>
           Create board
         </Button>
       </div>
