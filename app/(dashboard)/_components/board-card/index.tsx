0d7a63454e43d6e5f2515b794473c8179c639215
TatsuakiIto <gratachu9@gmail.com>
2024-03-25 20:30:45 +0900

    add: board list

---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import Link from "next/link";
+import Image from "next/image";
+
+interface BoardCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  authorId: string;
+  authorName: string;
+  createdAt: number;
+  orgId: string;
+  isFavorite: boolean;
+}
+
+export const BoardCard = ({
+  id,
+  title,
+  imageUrl,
+  authorId,
+  authorName,
+  createdAt,
+  orgId,
+  isFavorite
+}: BoardCardProps) => {
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+        <div className="relative flex-1 bg-amber-50">
+          <Image
+            src={imageUrl}
+            alt="Doodle"
+            fill
+            className="object-fit"
+          />
+        </div>
+      </div>
+    </Link>
+  )
+}
