736b748c0fe966e2e42d6e79f302aa71921cb46e
TatsuakiIto <gratachu9@gmail.com>
2024-03-31 18:35:59 +0900

    add: more action button to each board

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -8,6 +8,8 @@ import {formatDistanceToNow} from "date-fns";
 import {Footer} from "@/app/(dashboard)/_components/board-card/footer";
 
 import { Skeleton } from "@/components/ui/skeleton"
+import {Actions} from "@/components/actions";
+import {MoreHorizontal} from "lucide-react";
 
 interface BoardCardProps {
   id: string;
@@ -45,6 +47,17 @@ export const BoardCard = ({
             fill
             className="object-fit"
           />
+          <Actions
+            id={id}
+            title={title}
+            side="right"
+          >
+            <button className="absolute z-50 top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal
+                className="text-white opacity-75 hover:opacity-100 transition-opacity"
+              />
+            </button>
+          </Actions>
           <Overlay />
         </div>
         <Footer
