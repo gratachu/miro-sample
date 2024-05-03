f578d510ce81af17f221d4c9ea99362c14fcb66e
TatsuakiIto <gratachu9@gmail.com>
2024-05-03 11:04:07 +0900

    Add: canvas type for toolbar

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -35,8 +35,10 @@ export const Toolbar = ({
         <ToolButton
           label="Text"
           icon={Type}
-          onClick={() => {}}
-          isActive={false}
+          onClick={() => setCanvasState({ mode: CanvasMode.Inserting })}
+          isActive={
+            canvasState.mode === CanvasMode.Inserting
+          }
         />
         <ToolButton
           label="Sticky note"
--- types/canvas.ts
+++ types/canvas.ts
@@ -1,8 +1,33 @@
 export type CanvasState =
   | {
-  mode: CanvasMode.None;
+    mode: CanvasMode.None;
     }
+  | {
+    mode: CanvasMode.SelectionNet;
+    }
+  | {
+    mode: CanvasMode.Translating;
+    }
+  | {
+    mode: CanvasMode.Inserting;
+    }
+  | {
+    mode: CanvasMode.Resizing;
+    }
+  | {
+    mode: CanvasMode.Pencil;
+    }
+  | {
+    mode: CanvasMode.Pressing;
+    }
+
 
 export enum CanvasMode {
   None,
+  Pressing,
+  SelectionNet,
+  Translating,
+  Inserting,
+  Resizing,
+  Pencil
 }
